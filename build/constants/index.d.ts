4b26c5dfdbc5b697154cd49af8bd958e413eec5d
nateweller <nateweller@users.noreply.github.com>
2024-11-26 17:33:26 +0000

    Scan Package: Fix Build (#40299)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12035755070
    
    Upstream-Ref: Automattic/jetpack@70a19bc378a608a8de6de43bd03e29026df31a53

---
+++ build/constants/index.d.ts
@@ -0,0 +1,3 @@
+export declare const CONTACT_SUPPORT_URL = "https://jetpack.com/contact-support/?rel=support";
+/** Once a fixer has been running for this specified amount of time (in ms), it should be considered "stale". */
+export declare const FIXER_IS_STALE_THRESHOLD: number;
---
+++ build/utils/index.d.ts
@@ -0,0 +1,15 @@
+import { type ThreatFixStatus } from '../types/fixers.js';
+import { type Threat } from '../types/threats.js';
+export declare const getThreatType: (threat: Threat) => "plugin" | "theme" | "core" | "file";
+export declare const fixerTimestampIsStale: (lastUpdatedTimestamp: string) => boolean;
+export declare const fixerIsInError: (fixerStatus: ThreatFixStatus) => boolean;
+export declare const fixerIsInProgress: (fixerStatus: ThreatFixStatus) => boolean;
+export declare const fixerStatusIsStale: (fixerStatus: ThreatFixStatus) => boolean;
+export declare const getFixerState: (fixerStatus: ThreatFixStatus) => {
+    inProgress: boolean;
+    error: boolean;
+    stale: boolean;
+};
+export declare const getFixerAction: (threat: Threat) => string;
+export declare const getDetailedFixerAction: (threat: Threat) => string;
+export declare const getFixerDescription: (threat: Threat) => string;
