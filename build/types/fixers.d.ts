b6d456bd0795477b78ba0d2866978c6c9c31e824
nateweller <nateweller@users.noreply.github.com>
2024-12-03 03:55:08 +0000

    Scan Package: Additional Types from Protect (#40399)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12132489295
    
    Upstream-Ref: Automattic/jetpack@d9c0008668906e5153dc07116703535f1fc8210c

--- build/types/fixers.d.ts
+++ build/types/fixers.d.ts
@@ -12,3 +12,23 @@ export type ThreatFixStatusSuccess = {
     lastUpdated: string;
 };
 export type ThreatFixStatus = ThreatFixStatusError | ThreatFixStatusSuccess;
+/**
+ * Fixers Status
+ *
+ * Overall status of all fixers.
+ */
+type FixersStatusBase = {
+    ok: boolean;
+};
+export type FixersStatusError = FixersStatusBase & {
+    ok: false;
+    error: string;
+};
+export type FixersStatusSuccess = FixersStatusBase & {
+    ok: true;
+    threats: {
+        [key: number]: ThreatFixStatus;
+    };
+};
+export type FixersStatus = FixersStatusSuccess | FixersStatusError;
+export {};
