70d2fdc136356a6fb26e5e84aa5b3b4c3648ac77
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-08-18 15:13:03 +0000

    Add typecheck for protect plugin and fix errors (#44787)
    * Add typecheck for protect plugin and fix errors
    
    * Remove unsed comment
    
    * Avoid type-checking other workspaces via node_modules
    
    * Update use-scan-status-query.ts
    
    * Fix more type errors
    
    * Remove unnecessary optional chaining
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17044223987
    
    Upstream-Ref: Automattic/jetpack@1a105cdd35cb129e09b63408fc83704140828bac

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 This is an alpha version! The changes listed here are not final.
 
+### Added
+- Added missing types for protect plugin.
+
 ### Changed
 - Update @wordpress/dataviews to 5.0.0
 - Update package dependencies.
--- build/types/status.d.ts
+++ build/types/status.d.ts
@@ -1,4 +1,18 @@
 import { Threat } from './threats.ts';
+export type ExtensionStatus = {
+    /** The name of the extension. */
+    name: string;
+    /** The slug of the extension. */
+    slug: string;
+    /** The version of the extension. */
+    version: string;
+    /** The threats found in the extension. */
+    threats: Threat[];
+    /** The type of extension. */
+    type: 'plugins' | 'themes';
+    /** Whether the extension was checked in the latest scan. */
+    checked: boolean;
+};
 export type ScanStatus = {
     /** The current status of the scanner. */
     status: 'unavailable' | 'provisioning' | 'idle' | 'scanning' | 'scheduled';
@@ -20,4 +34,9 @@ export type ScanStatus = {
     errorMessage: string | null;
     /** The detected threats. */
     threats: Threat[];
+    core: ExtensionStatus | ExtensionStatus[];
+    plugins: ExtensionStatus[];
+    themes: ExtensionStatus[];
+    files: Threat[];
+    database: Threat[];
 };
